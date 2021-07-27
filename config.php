a6bc39cd38e5fa483b65e400253fb0ecdebcb595
Armeizir <armeizir@gmail.com>
2021-07-27 21:16:25 +0700

    update

---
+++ config.php
@@ -0,0 +1,16 @@
+<? 
+    session_start();
+    $db_host = "localhost";
+    $db_username = "root";
+    $db_password = "";
+    $db_name = "sekolah";
+    $conn = mysql_connect($db_host, $db_username, $db_password) or die(mysql_error());
+    $db = mysql_select_db($db_name, $conn) or die(mysql_error());
+    
+    //Connect and select the database
+    $db1 = new mysqli($db_host, $db_username, $db_password, $db_name);
+    
+    if ($db1->connect_error) {
+        die("Connection failed: " . $db1->connect_error);
+    }
+?>
---
+++ setting.php
@@ -0,0 +1,8 @@
+<?
+    ob_start();
+    include"config.php";
+    
+    $qConfig = "select * from `config`  where `config_id` = '1'";
+    $rConfig = mysql_query($qConfig, $conn) or die(mysql_error());
+    $aConfig = mysql_fetch_array($rConfig);
+?>
