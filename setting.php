fa7595724c12a17af0db6e9439a91490ab9d113e
Armeizir <armeizir@gmail.com>
2021-07-30 14:23:28 +0700

    test

--- setting.php
+++ setting.php
@@ -5,4 +5,8 @@
     $qConfig = "select * from `config`  where `config_id` = '1'";
     $rConfig = mysql_query($qConfig, $conn) or die(mysql_error());
     $aConfig = mysql_fetch_array($rConfig);
+
+    $qWargaGlobal = "select * from `warga` where `warga_id` = '$_SESSION[WargaID]'";
+    $rWargaGlobal = mysql_query($qWargaGlobal, $conn) or die(mysql_error());
+    $aWargaGlobal = mysql_fetch_array($rWargaGlobal);
 ?>
